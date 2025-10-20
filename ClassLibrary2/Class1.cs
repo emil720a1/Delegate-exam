be30a3a10ebdd9aa89990549ed2e9be0c3cdff50
mcaleg2006 <emil720apro@gmail.com>
2025-10-20 10:52:03 +0300

    delegate

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassLibrary2;
+
+public delegate void Notify(string message);
+
+class Notifer
+{
+    public event Notify? _Notify;
+    public void Send(string message)
+    {
+        _Notify?.Invoke(message);
+    }
+}
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        var notify = new Notifer();
+        notify._Notify += (msg) => Console.WriteLine($"Log: {msg}");
+        notify.Send("Hello");
+        notify._Notify += (msg) => Console.WriteLine($"Email: {msg}");
+        notify.Send("@gmail.com");
+        
+        
+        
+    }
+
+    static void ShowMsg(string s)
+    {
+        Console.WriteLine(s);
+    }
+
+}
