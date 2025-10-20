13c3ae8af06c2a0c7d9857e5fa73b6c7f400c346
mcaleg2006 <emil720apro@gmail.com>
2025-10-20 11:01:56 +0300

    Action & Compare exam

--- ClassLibrary2/Class1.cs
+++ ClassLibrary2/Class1.cs
@@ -2,33 +2,36 @@
 
 public delegate void Notify(string message);
 
-class Notifer
-{
-    public event Notify? _Notify;
-    public void Send(string message)
-    {
-        _Notify?.Invoke(message);
-    }
-}
+// class Notifer
+// {
+//     public event Notify? _Notify;
+//     public void Send(string message)
+//     {
+//         _Notify?.Invoke(message);
+//     }
+// }
 
 class Program
 {
 
     static void Main(string[] args)
     {
-        var notify = new Notifer();
-        notify._Notify += (msg) => Console.WriteLine($"Log: {msg}");
-        notify.Send("Hello");
-        notify._Notify += (msg) => Console.WriteLine($"Email: {msg}");
-        notify.Send("@gmail.com");
+        // var notify = new Notifer();
+        // notify._Notify += (msg) => Console.WriteLine($"Log: {msg}");
+        // notify.Send("Hello");
+        // notify._Notify += (msg) => Console.WriteLine($"Email: {msg}");
+        // notify.Send("@gmail.com");
         
+        Action<int> print = (y) => Console.WriteLine(y);
+        List<int> numbers = new() {1, 2 ,3, 4, 5, 6, 7, 8, 9, 10};
         
-        
+        numbers.Sort((a, b) => b.CompareTo(a));
+        numbers.ForEach(print);
     }
 
-    static void ShowMsg(string s)
-    {
-        Console.WriteLine(s);
-    }
+    // static void ShowMsg(string s)
+    // {
+    //     Console.WriteLine(s);
+    // }
 
 }
